2824b61736854c3db1cfd94cd27dc8c4a2d4d948
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2025-05-15 11:12:26 +0800

    Create sk.py
    操作计算器

---
+++ sk.py
@@ -0,0 +1,9 @@
+# https://blog.csdn.net/abu935009066/article/details/131328693
+
+type("r",Key.WIN)  #按WIN+R健
+paste("calc")
+type(Key.ENTER)
+wait(2)
+paste("1+2")
+wait(1)
+click("1747277886204.png")
