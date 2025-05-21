55e57cc6e2893df3c29507329854b2d622e89102
Jimmy Yue <53287360+jimmyue@users.noreply.github.com>
2025-05-21 17:53:28 +0800

    Update sk.py

--- sk.py
+++ sk.py
@@ -1,9 +1,18 @@
 # https://blog.csdn.net/abu935009066/article/details/131328693
 
+import test # 导入同目录python脚本(PYTHON的包sikulix不能直接导入，所以要用python写)
+
+# 调用特定函数并传参
+result_add = test.int_add("5","4")
+result_multiply = test.int_multiply("5","4")
+result_str=test.str_add("ca", "lc")
+
+print(result_add,result_multiply,result_str)     
+
 type("r",Key.WIN)  #按WIN+R健
-paste("calc")
+paste(result_str)
 type(Key.ENTER)
 wait(2)
 paste("1+2")
 wait(1)
-click("1747277886204.png")
+click("1747821068723.png")
